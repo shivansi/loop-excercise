4a41977cce243021b37f9ffc4af10ec2d2576446
S B <barat.shivansi@gmail.com>
2020-08-05 21:11:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+#1
+#1 2
+#1 2 3
+#1 2 3 4
+#1 2 3 4 5
+
+# for i in range(1,7):
+#   for j in range(1,i):
+#     print(j,end="")
+#   print()
+
+# for i in range(1,5):
+#   print(i)
+
+# for b in range(5):
+#   for  in range(4):
+#     print("hello")
+
+num=input("enter a number")
+
+if float(num) == 0:
+  print("number is zero!")
+elif float(num) > 1:
+  print("the number is positive!")
+else:
+  print("number is negative")
+
+#conditional to assess wether num is positive, negative, or zero
+
